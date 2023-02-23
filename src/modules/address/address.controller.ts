213a353240cc15829b8f9c4884761bd0d78baee0
李秋秋 <2684196398@qq.com>
2023-02-23 23:22:15 +0800

    feat: :sparkles: 新增根据id查询所有祖先地址节点接口

--- src/modules/address/address.controller.ts
+++ src/modules/address/address.controller.ts
@@ -2,8 +2,11 @@ import {
   ApiForbiddenResponse,
   ApiPaginatedResponse,
   ApiResponse,
+  ApiResponseArray,
   ApiUnauthorizedResponse,
+  Auth,
 } from '@/common/decorators';
+import { Role } from '@/common/enum/role.enum';
 import { ParsePositiveIntPipe } from '@/common/pipe/parse-positive-int.pipe';
 import {
   Controller,
@@ -26,7 +29,6 @@ import { Address } from './entities/address.entity';
 @ApiTags('address')
 @ApiForbiddenResponse()
 @ApiUnauthorizedResponse()
-// @Auth(Role.Admin)
 @Controller('address')
 export class AddressController {
   constructor(private readonly addressService: AddressService) {}
@@ -38,11 +40,24 @@ export class AddressController {
     return this.addressService.create(createAddressDto);
   }
 
+  @ApiParam({ type: 'integer', name: 'addressId' })
   @ApiOperation({
-    summary: '根据父级id查询所有节点',
+    summary: '根据id查询所有祖先节点',
+    operationId: 'findAncestorsById',
+  })
+  @ApiResponseArray(Address)
+  @Auth()
+  @Get('ancestors/:addressId')
+  findAncestors(@Param('addressId', ParsePositiveIntPipe) id: number) {
+    return this.addressService.findAncestors(id);
+  }
+
+  @ApiOperation({
+    summary: '查询所有地址',
     operationId: 'getAllAddress',
   })
   @ApiPaginatedResponse(Address)
+  @Auth()
   @Get()
   findAll(@Query() queryAddressDto: QueryAddressDto) {
     return this.addressService.findAll(queryAddressDto);
@@ -51,8 +66,8 @@ export class AddressController {
   @ApiOperation({ summary: '根据id更新地址', operationId: 'updateAddressById' })
   @ApiParam({ type: 'integer', name: 'addressId' })
   @ApiResponse(Address)
+  @Auth(Role.Admin)
   @Patch(':addressId')
-  // @Put(':addressId')
   update(
     @Param('addressId', ParsePositiveIntPipe) id: number,
     @Body() updateAddressDto: UpdateAddressDto,
@@ -60,18 +75,22 @@ export class AddressController {
     return this.addressService.update(id, updateAddressDto);
   }
 
+  @ApiParam({ type: 'integer', name: 'addressId' })
   @ApiResponse(Address)
   @ApiOperation({ summary: '根据id删除地址', operationId: 'deleteAddressById' })
+  @Auth(Role.Admin)
   @Delete(':addressId')
   remove(@Param('addressId', ParsePositiveIntPipe) id: number) {
     return this.addressService.remove(id);
   }
 
+  @ApiParam({ type: 'integer', name: 'addressId' })
   @ApiResponse(Address)
   @ApiOperation({
     summary: '根据id恢复地址',
     operationId: 'recoverAddressById',
   })
+  @Auth(Role.Admin)
   @Patch('/recover/:addressId')
   recover(@Param('addressId', ParsePositiveIntPipe) id: number) {
     return this.addressService.recover(id);
